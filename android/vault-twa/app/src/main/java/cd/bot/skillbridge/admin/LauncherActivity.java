c6c4560465b772ce025f12f571bc93bb86e087ee
Aniket Gupta <124371179+aniket-gupta020@users.noreply.github.com>
2026-09-05 18:40:07 +0530

    fix(twa): integrate biometric prompt inside LauncherActivity and configure standalone Netlify assetlinks

--- android/vault-twa/app/src/main/java/cd/bot/skillbridge/admin/LauncherActivity.java
+++ android/vault-twa/app/src/main/java/cd/bot/skillbridge/admin/LauncherActivity.java
@@ -1,54 +1,151 @@
-/*
- * Copyright 2020 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cd.bot.skillbridge.admin;
 
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
+import android.hardware.biometrics.BiometricPrompt;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-
-
+import android.os.CancellationSignal;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.biometric.BiometricManager;
+import androidx.core.content.ContextCompat;
 
 public class LauncherActivity
         extends com.google.androidbrowserhelper.trusted.LauncherActivity {
-    
 
-    
+    private static final String TAG = "VaultLauncher";
+    private static final String KEY_IS_AUTHENTICATED = "vault_is_authenticated";
+    private boolean mIsAuthenticated = false;
+    private CancellationSignal mCancellationSignal;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Setting an orientation crashes the app due to the transparent background on Android 8.0
-        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
-        // splash screen and Chrome will still respect the orientation.
-        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
+
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         } else {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         }
+
+        if (savedInstanceState != null) {
+            mIsAuthenticated = savedInstanceState.getBoolean(KEY_IS_AUTHENTICATED, false);
+        }
+
+        if (!mIsAuthenticated) {
+            checkAndPromptBiometrics();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_IS_AUTHENTICATED, mIsAuthenticated);
+    }
+
+    @Override
+    protected boolean shouldLaunchImmediately() {
+        return mIsAuthenticated;
+    }
+
+    private void checkAndPromptBiometrics() {
+        BiometricManager biometricManager = BiometricManager.from(this);
+        int authenticators = BiometricManager.Authenticators.BIOMETRIC_STRONG | BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+        int canAuthenticate = biometricManager.canAuthenticate(authenticators);
+
+        if (canAuthenticate == BiometricManager.BIOMETRIC_SUCCESS && Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            showBiometricPrompt();
+        } else {
+            // Biometrics not enrolled, hardware not present, or legacy OS: proceed to launch TWA directly
+            mIsAuthenticated = true;
+            launchTwa();
+        }
+    }
+
+    private void showBiometricPrompt() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            mIsAuthenticated = true;
+            launchTwa();
+            return;
+        }
+
+        mCancellationSignal = new CancellationSignal();
+
+        BiometricPrompt.Builder builder = new BiometricPrompt.Builder(this)
+                .setTitle(getString(R.string.biometricTitle))
+                .setSubtitle(getString(R.string.biometricSubtitle))
+                .setDescription(getString(R.string.biometricDesc));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            builder.setAllowedAuthenticators(
+                    android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG |
+                    android.hardware.biometrics.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+            );
+        } else {
+            builder.setNegativeButton(
+                    "Cancel",
+                    ContextCompat.getMainExecutor(this),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }
+            );
+        }
+
+        BiometricPrompt biometricPrompt = builder.build();
+
+        biometricPrompt.authenticate(
+                mCancellationSignal,
+                ContextCompat.getMainExecutor(this),
+                new BiometricPrompt.AuthenticationCallback() {
+                    @Override
+                    public void onAuthenticationError(int errorCode, CharSequence errString) {
+                        super.onAuthenticationError(errorCode, errString);
+                        Log.d(TAG, "Biometric error: " + errorCode + " - " + errString);
+                        if (errorCode != BiometricPrompt.BIOMETRIC_ERROR_USER_CANCELED &&
+                            errorCode != BiometricPrompt.BIOMETRIC_ERROR_CANCELED) {
+                            Toast.makeText(LauncherActivity.this, "Authentication: " + errString, Toast.LENGTH_SHORT).show();
+                        }
+                        if (!isFinishing()) {
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                        super.onAuthenticationSucceeded(result);
+                        Log.d(TAG, "Biometric authentication succeeded");
+                        mIsAuthenticated = true;
+                        if (!isFinishing()) {
+                            launchTwa();
+                        }
+                    }
+
+                    @Override
+                    public void onAuthenticationFailed() {
+                        super.onAuthenticationFailed();
+                        Log.d(TAG, "Biometric attempt failed; awaiting retry");
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mCancellationSignal != null && !mCancellationSignal.isCanceled()) {
+            mCancellationSignal.cancel();
+        }
+        super.onDestroy();
     }
 
     @Override
     protected Uri getLaunchingUrl() {
-        // Get the original launch Url.
-        Uri uri = super.getLaunchingUrl();
-
-        
-
-        return uri;
+        return super.getLaunchingUrl();
     }
 }
